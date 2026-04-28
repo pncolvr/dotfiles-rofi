f37ce4586a288932e1be506ab3d5bffe8887b6eb
Pedro Oliveira <pncolvr@gmail.com>
2026-04-28 19:06:14 +0100

    feat: webapps support runing actions before and after opening the webapp

--- scripts/web/webapps.sh
+++ scripts/web/webapps.sh
@@ -6,10 +6,15 @@ source "$WORKSPACE"/utils.sh
 links=$(realpath $(get_env_file ${BASH_SOURCE[0]:-0}))
 selection=$("$WORKSPACE"/handle.sh "$links" )
 
-IFS=';' read action url <<< "$(jq -r '[.action, .url] | join(";")' <<< $selection)"
+IFS=';' read action url execute_before execute_after <<< "$(jq -r '[.action, .url, .executeBefore, .executeAfter] | join(";")' <<< $selection)"
 
 if [[ -z "$action" || -z "$url" ]]; then
     exit
+fi
+
+if [[ -n "$execute_before" ]]; then
+    echo "executing $execute_before"
+    eval "$execute_before"
 fi
 
 case "$action" in
@@ -17,5 +22,10 @@ case "$action" in
         url="$action $url" 
         ;;
 esac
+
+if [[ -n "$execute_after" ]]; then
+    echo "executing $execute_after"
+    eval "$execute_after"
+fi
 
 $ZDOTDIR/scripts/default-browser/default-browser.sh $(printf '%s' "$url")
